4c2aca213c1b305a10851a35aa1bc1fd56001417
Herbert Milhomme <herb@herbertmilhomme.com>
2018-06-04 10:03:51 -0400

    Modified ToString Return
    I added small comma here and there in the first part, and redid everything over again in the 2nd part.
    Not sure how much is broken. I didnt do any testing. Please double check my counting on `string.Format`, i'm not sure if it's really 33 values...
    
    The idea is, if it doesnt have any value, it doesnt need to be there. So i added a null check, and then told it to skip. I added commas before each value. Added a splash of `\n`, for clarity and readability. They arent a lot, and they're not needed. (So they can easily be removed)

--- PokemonDatabase/Source Files/CSVDatabaseReader/CSVDatabaseReader/Pokemon.cs
+++ PokemonDatabase/Source Files/CSVDatabaseReader/CSVDatabaseReader/Pokemon.cs
@@ -94,14 +94,14 @@ namespace CSVDatabaseReader
 
         public override string ToString()
         {
-            return string.Format($"new PokemonData( Id: Pokemons.{NAME.ToUpper()}, regionalDex: new int[{RegionalDex}], type1: Types.{Type1}, type2: Types.{Type2},\n" +
+            return string.Format($"new PokemonData( Id: Pokemons.{NAME.ToUpper()}, regionalDex: new int[]{ {RegionalDex} }, type1: Types.{Type1}, type2: Types.{Type2},\n" +
                 $"ability1: Abilities.{Ability1}, ability2: Abilities.{Ability2}, hiddenAbility: Abilities.{HiddenAbility},\n" +
                 $"maleRatio: {MaleRatio}, catchRate: {CatchRate}, eggGroup1: EggGroups.{EggGroup1}, eggGroup2: EggGroups.{EggGroup2}, hatchTime: {HatchTime},\n" +
                 $"height: {Height}, weight: {Weight}, baseExpYield: {EXPYield}, levelingRate: LevelingRate.{LevelingRate},\n" +
                 $"pokedexColor: Color.{PokedexColor}, baseFriendship: {BaseFriendship},\n" +
                 $"baseStatsHP: {BSHP}, baseStatsATK: {BSATK}, baseStatsDEF: {BSDEF}, baseStatsSPA: {BSSPA}, baseStatsSPD: {BSSPD}, baseStatsSPE: {BSSPE},\n" +
                 $"effortValuesHP: {EYHP}, effortValuesATK: {EYATK}, effortValuesDEF: {EYDEF}, effortValuesSPA: {EYSPA}, effortValuesSPD: {EYSPD}, effortValues: {EYSPE},\n" +
-                $"luminance: {Luminance},\n" +
+                //$"luminance: {Luminance},\n" +
                 @"movesetmoves: new PokemonMoveset[] " +
                 $"\n" +
                 @"{{" +
@@ -112,7 +112,202 @@ namespace CSVDatabaseReader
                 @"evolution: new IPokemonEvolution[] {{" +
                 $"{PokemonEvolution}" +
                 @"}}," +
-                $"\nheldItem: {HeldItem})");
+                //$"\nheldItem: {HeldItem}
+			 ),");
+        }
+
+        public override string ToString(1)
+        {
+            return string.Format("new PokemonData(" 
+								 Id: Pokemons.{NAME.ToUpper()}, regionalDex: new int[]{ {RegionalDex} }, type1: Types.{Type1}, type2: Types.{Type2},\n" +
+                $"ability1: Abilities.{Ability1}, ability2: Abilities.{Ability2}, hiddenAbility: Abilities.{HiddenAbility},\n" +
+                $"maleRatio: {MaleRatio}, catchRate: {CatchRate}, eggGroup1: EggGroups.{EggGroup1}, eggGroup2: EggGroups.{EggGroup2}, hatchTime: {HatchTime},\n" +
+                $"height: {Height}, weight: {Weight}, baseExpYield: {EXPYield}, levelingRate: LevelingRate.{LevelingRate},\n" +
+                $"pokedexColor: Color.{PokedexColor}, baseFriendship: {BaseFriendship},\n" +
+                $"baseStatsHP: {BSHP}, baseStatsATK: {BSATK}, baseStatsDEF: {BSDEF}, baseStatsSPA: {BSSPA}, baseStatsSPD: {BSSPD}, baseStatsSPE: {BSSPE},\n" +
+                $"effortValuesHP: {EYHP}, effortValuesATK: {EYATK}, effortValuesDEF: {EYDEF}, effortValuesSPA: {EYSPA}, effortValuesSPD: {EYSPD}, effortValues: {EYSPE},\n" +
+                //$"luminance: {Luminance},\n" +
+                @"movesetmoves: new PokemonMoveset[] " +
+                $"\n" +﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace CSVDatabaseReader
+{
+    class Pokemon
+    {
+        public string RegionalDex { get; set; }     //Done
+        public string ID { get; set; }              //Done
+        public string NAME { get; set; }            //Done
+        public enum Types
+        {
+            NONE = 0,
+            NORMAL = 1,
+            FIGHTING = 2,
+            FLYING = 3,
+            POISON = 4,
+            GROUND = 5,
+            ROCK = 6,
+            BUG = 7,
+            GHOST = 8,
+            STEEL = 9,
+            FIRE = 10,
+            WATER = 11,
+            GRASS = 12,
+            ELECTRIC = 13,
+            PSYCHIC = 14,
+            ICE = 15,
+            DRAGON = 16,
+            DARK = 17,
+            FAIRY = 18,
+            UNKNOWN = 10001,
+            SHADOW = 10002
+        }
+        public string Type1 { get; set; }           //Done
+        public string Type2 { get; set; }           //Done
+
+        public string Ability1 { get; set; }        //Done
+        public string Ability2 { get; set; }        //Done
+        public string HiddenAbility { get; set; }   //Done
+
+        public string MaleRatio { get; set; }       //Done
+
+        public string CatchRate { get; set; }       //Done
+
+        public string EggGroup1 { get; set; }       //Done
+        public string EggGroup2 { get; set; }       //Done
+
+        public string HatchTime { get; set; }       //Done
+
+        public string Height { get; set; }          //Done
+        public string Weight { get; set; }          //Done
+
+        public string EXPYield { get; set; }        //Done
+        public string LevelingRate { get; set; }    //Done
+
+        public string EYHP { get; set; }            //Done
+        public string EYATK { get; set; }           //Done
+        public string EYDEF { get; set; }           //Done
+        public string EYSPA { get; set; }           //Done
+        public string EYSPD { get; set; }           //Done
+        public string EYSPE { get; set; }           //Done
+
+        public string PokedexColor { get; set; }    //Done
+
+        public string BaseFriendship { get; set; }   //Done
+
+        public string Species { get; set; }         //Done
+
+        public string Description { get; set; }     //Done
+
+        public string BSHP { get; set; }            //Done
+        public string BSATK { get; set; }           //Done
+        public string BSDEF { get; set; }           //Done
+        public string BSSPA { get; set; }           //Done
+        public string BSSPD { get; set; }           //Done
+        public string BSSPE { get; set; }           //Done
+
+        public string Luminance { get; set; }       //not needed
+        public string LightColor { get; set; }      //Done
+
+        public string LevelMoves { get; set; }
+        public string TMMoves { get; set; }
+
+        public string PokemonEvolution { get; set; }//Done
+        public string LevelEvolution { get; set; }  //Done
+
+        public string Forms { get; set; }			//manually
+        public string HeldItem { get; set; }		//manually
+
+        public override string ToString()
+        {
+            return string.Format($"new PokemonData( Id: Pokemons.{NAME.ToUpper()}, regionalDex: new int[]{ {RegionalDex} }, type1: Types.{Type1}, type2: Types.{Type2},\n" +
+                $"ability1: Abilities.{Ability1}, ability2: Abilities.{Ability2}, hiddenAbility: Abilities.{HiddenAbility},\n" +
+                $"maleRatio: {MaleRatio}, catchRate: {CatchRate}, eggGroup1: EggGroups.{EggGroup1}, eggGroup2: EggGroups.{EggGroup2}, hatchTime: {HatchTime},\n" +
+                $"height: {Height}, weight: {Weight}, baseExpYield: {EXPYield}, levelingRate: LevelingRate.{LevelingRate},\n" +
+                $"pokedexColor: Color.{PokedexColor}, baseFriendship: {BaseFriendship},\n" +
+                $"baseStatsHP: {BSHP}, baseStatsATK: {BSATK}, baseStatsDEF: {BSDEF}, baseStatsSPA: {BSSPA}, baseStatsSPD: {BSSPD}, baseStatsSPE: {BSSPE},\n" +
+                $"effortValuesHP: {EYHP}, effortValuesATK: {EYATK}, effortValuesDEF: {EYDEF}, effortValuesSPA: {EYSPA}, effortValuesSPD: {EYSPD}, effortValues: {EYSPE},\n" +
+                //$"luminance: {Luminance},\n" +
+                @"movesetmoves: new PokemonMoveset[] " +
+                $"\n" +
+                @"{{" +
+                $"{LevelMoves}\n" +
+                $"{TMMoves}\n" +
+                @"}}," +
+                $"\n" +
+                @"evolution: new IPokemonEvolution[] {{" +
+                $"{PokemonEvolution}" +
+                @"}}," +
+                //$"\nheldItem: {HeldItem}
+			 ),");
+        }
+		
+		///<summary>Int value doesnt matter, ex `.ToString(1);`</summary>
+        public override string ToString(int newVersion)
+        {
+            return string.Format("new PokemonData({0} {1} {2} {3} {4} {5} {6} {7} {8} {9} " +
+												"{10} {11} {12} {13} {14} {15} {16} {17} {18} {19} " +
+												"{20} {21} {22} {23} {24} {25} {26} {27} {28} {29} " +
+												"{30} {31} {32})",
+				NAME == null? "" :				"Id: Pokemons."+NAME.ToUpper(), 
+				RegionalDex == null? "" :		",\nregionalDex: new int[]{"+RegionalDex+"}", 
+				Type1 == null? "" :				",\ntype1: Types."+Type1, 
+				Type2 == null? "" :				",\ntype2: Types."+Type2,
+                Ability1 == null? "" :			",\nability1: Abilities."+Ability1, 
+				Ability2 == null? "" :			",\nability2: Abilities."+Ability2, 
+				HiddenAbility == null? "" :		",\nhiddenAbility: Abilities."+HiddenAbility,
+                MaleRatio == null? "" :			",\nmaleRatio: "+MaleRatio, 
+				CatchRate == null? "" :			",\ncatchRate: "+CatchRate, 
+				EggGroup1 == null? "" :			",\neggGroup1: EggGroups."+EggGroup1, 
+				EggGroup2 == null? "" :			",\neggGroup2: EggGroups."+EggGroup2, 
+				HatchTime == null? "" :			",\nhatchTime: "+HatchTime,
+                Height == null? "" :			",\nheight: "+Height, 
+				Weight == null? "" :			",\nweight: "+Weight, 
+				LevelingRate == null? "" :		",\nlevelingRate: LevelingRate."+LevelingRate,//\n" +
+                PokedexColor == null? "" :		",\npokedexColor: Color."+PokedexColor, 
+				BaseFriendship == null? "" :	",\nbaseFriendship: "+BaseFriendship,
+				EXPYield == null? "" :			",\nbaseExpYield: "+EXPYield, 
+                BSHP == null? "" :				",baseStatsHP: "+BSHP, 
+				BSATK == null? "" :				",baseStatsATK: "+BSATK, 
+				BSDEF == null? "" :				",baseStatsDEF: "+BSDEF, 
+				BSSPA == null? "" :				",baseStatsSPA: "+BSSPA, 
+				BSSPD == null? "" :				",baseStatsSPD: "+BSSPD, 
+				BSSPE == null? "" :				",baseStatsSPE: "+BSSPE,//\n" +
+                EYHP == null? "" :				",\neffortValuesHP: "+EYHP, 
+				EYATK == null? "" :				",evATK: "+EYATK, 
+				EYDEF == null? "" :				",evDEF: "+EYDEF, 
+				EYSPA == null? "" :				",evSPA: "+EYSPA, 
+				EYSPD == null? "" :				",evSPD: "+EYSPD, 
+				EYSPE == null? "" :				",evSPE: "+EYSPE,//\n" +
+                //NAME == null? "" : "$"luminance: "+Luminance,\n" +
+                ",movesetmoves: new PokemonMoveset[] " +
+                //$"\n" +
+                "{" +
+                LevelMoves + "\n" +
+                TMMoves + "\n" +
+                "}",
+                //$"\n" +
+                PokemonEvolution == null? "" : ",evolution: new IPokemonEvolution[] {" + PokemonEvolution +"},"
+                //,HeldItem == null? "" : "heldItem: "+HeldItem +","
+			 );
+        }
+    }
+}
+
+                @"{{" +
+                $"{LevelMoves}\n" +
+                $"{TMMoves}\n" +
+                @"}}," +
+                $"\n" +
+                @"evolution: new IPokemonEvolution[] {{" +
+                $"{PokemonEvolution}" +
+                @"}}," +
+                //$"\nheldItem: {HeldItem}
+			 ),");
         }
     }
 }
